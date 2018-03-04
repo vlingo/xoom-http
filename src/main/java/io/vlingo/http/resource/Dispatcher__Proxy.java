e42deb95969d91ce7a639105e4a1fb76cf949b23
Vaughn Vernon <vaughn@forcomprehension.com>
2018-03-04 13:18:38 +0100

    Created Dispatcher__Proxy to prevent generation.

---
+++ src/main/java/io/vlingo/http/resource/Dispatcher__Proxy.java
@@ -0,0 +1,56 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.http.resource;
+
+import java.util.function.Consumer;
+
+import io.vlingo.actors.Actor;
+import io.vlingo.actors.DeadLetter;
+import io.vlingo.actors.LocalMessage;
+import io.vlingo.actors.Mailbox;
+
+public class Dispatcher__Proxy implements Dispatcher {
+
+  private static final String dispatchForRepresentation1 = "dispatchFor(io.vlingo.http.Context)";
+  private static final String stopRepresentation2 = "stop()";
+  private static final String isStoppedRepresentation3 = "isStopped()";
+
+  private final Actor actor;
+  private final Mailbox mailbox;
+
+  public Dispatcher__Proxy(final Actor actor, final Mailbox mailbox){
+    this.actor = actor;
+    this.mailbox = mailbox;
+  }
+
+  public void dispatchFor(io.vlingo.http.Context arg0) {
+    if (!actor.isStopped()) {
+      final Consumer<Dispatcher> consumer = (actor) -> actor.dispatchFor(arg0);
+      mailbox.send(new LocalMessage<Dispatcher>(actor, Dispatcher.class, consumer, dispatchForRepresentation1));
+    } else {
+      actor.deadLetters().failedDelivery(new DeadLetter(actor, dispatchForRepresentation1));
+    }
+  }
+  public void stop() {
+    if (!actor.isStopped()) {
+      final Consumer<Dispatcher> consumer = (actor) -> actor.stop();
+      mailbox.send(new LocalMessage<Dispatcher>(actor, Dispatcher.class, consumer, stopRepresentation2));
+    } else {
+      actor.deadLetters().failedDelivery(new DeadLetter(actor, stopRepresentation2));
+    }
+  }
+  public boolean isStopped() {
+    if (!actor.isStopped()) {
+      final Consumer<Dispatcher> consumer = (actor) -> actor.isStopped();
+      mailbox.send(new LocalMessage<Dispatcher>(actor, Dispatcher.class, consumer, isStoppedRepresentation3));
+    } else {
+      actor.deadLetters().failedDelivery(new DeadLetter(actor, isStoppedRepresentation3));
+    }
+    return false;
+  }
+}
