6fdc1455bcfbf6feeb7b373625c2667db923ff5d
Vaughn Vernon <vaughn@forcomprehension.com>
2018-05-03 10:47:05 -0700

    Refactored test to only send a next request following an acknowledged response from the previous requests.

--- src/test/java/io/vlingo/http/resource/ServerTest.java
+++ src/test/java/io/vlingo/http/resource/ServerTest.java
@@ -30,7 +30,7 @@ import io.vlingo.wire.node.AddressType;
 import io.vlingo.wire.node.Host;
 
 public class ServerTest extends ResourceTestFixtures {
-  private static final int TOTAL_REQUESTS_RESPONSES = 1_000;
+  private static final int TOTAL_REQUESTS_RESPONSES = 10_000;
   
   private ClientRequestResponseChannel client;
   private ResponseChannelConsumer consumer;
@@ -81,9 +81,12 @@ public class ServerTest extends ResourceTestFixtures {
     progress.untilConsumed = TestUntil.happenings(TOTAL_REQUESTS_RESPONSES);
     final int totalPairs = TOTAL_REQUESTS_RESPONSES / 2;
     for (int idx = 0; idx < totalPairs; ++idx) {
+      final int currentConsumeCount = progress.consumeCount.get();
       client.requestWith(toByteBuffer(postRequest(uniqueJohnDoe())));
       client.requestWith(toByteBuffer(postRequest(uniqueJaneDoe())));
-      Thread.sleep(10);
+      while (progress.consumeCount.get() < currentConsumeCount) {
+        client.probeChannel();
+      }
     }
 
     while (progress.untilConsumed.remaining() > 0) {
