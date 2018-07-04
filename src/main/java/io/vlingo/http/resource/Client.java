f1db53f07adb9697eabb0072263a22a1ed5e1c53
Vaughn Vernon <vaughn@forcomprehension.com>
2018-07-04 11:15:47 -0700

    Implemented Client API for simple interface with network request/response using Completes<Response> for asynchronous response outcomes.

---
+++ src/main/java/io/vlingo/http/resource/Client.java
@@ -0,0 +1,207 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.http.resource;
+
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import io.vlingo.actors.Actor;
+import io.vlingo.actors.Cancellable;
+import io.vlingo.actors.Completes;
+import io.vlingo.actors.Definition;
+import io.vlingo.actors.Scheduled;
+import io.vlingo.actors.Stage;
+import io.vlingo.actors.Stoppable;
+import io.vlingo.http.Request;
+import io.vlingo.http.RequestHeader;
+import io.vlingo.http.Response;
+import io.vlingo.http.ResponseHeader;
+import io.vlingo.http.ResponseParser;
+import io.vlingo.wire.channel.ResponseChannelConsumer;
+import io.vlingo.wire.fdx.bidirectional.ClientRequestResponseChannel;
+import io.vlingo.wire.message.ByteBufferAllocator;
+import io.vlingo.wire.message.ConsumerByteBuffer;
+import io.vlingo.wire.message.Converters;
+import io.vlingo.wire.node.Address;
+import io.vlingo.wire.node.AddressType;
+import io.vlingo.wire.node.Host;
+
+public class Client {
+  private final ClientConsumer consumer;
+
+  public static Client using(final Configuration configuration) throws Exception {
+    return new Client(configuration);
+  }
+
+  public Client(final Configuration configuration) throws Exception {
+    this.consumer = configuration.stage.actorFor(
+            Definition.has(ClientRequesterConsumerActor.class, Definition.parameters(configuration)),
+            ClientConsumer.class);
+  }
+
+  public void close() {
+    consumer.stop();
+  }
+
+  public Completes<Response> requestWith(final Request request) {
+    return consumer.requestWith(request);
+  }
+
+  public static class Configuration {
+    public final Address addressOfHost;
+    public final ResponseConsumer consumerOfUnknownResponses;
+    public final long probeInterval;
+    public final int readBufferSize;
+    public final int readBufferPoolSize;
+    public final int writeBufferSize;
+    public final Stage stage;
+
+    public static Configuration defaultedExceptFor(
+            final Stage stage,
+            final ResponseConsumer consumerOfUnknownResponses) {
+      return new Configuration(
+              stage,
+              Address.from(Host.of("localhost"), 8080, AddressType.NONE),
+              consumerOfUnknownResponses,
+              10,
+              10240,
+              10,
+              10240);
+    }
+
+    public static Configuration has(
+            final Stage stage,
+            final Address addressOfHost,
+            final ResponseConsumer consumerOfUnknownResponses,
+            final long probeInterval,
+            final int writeBufferSize,
+            final int readBufferPoolSize,
+            final int readBufferSize) {
+      return new Configuration(
+              stage,
+              addressOfHost,
+              consumerOfUnknownResponses,
+              probeInterval,
+              writeBufferSize,
+              readBufferPoolSize,
+              readBufferSize);
+    }
+
+    public Configuration(
+            final Stage stage,
+            final Address addressOfHost,
+            final ResponseConsumer consumerOfUnknownResponses,
+            final long probeInterval,
+            final int writeBufferSize,
+            final int readBufferPoolSize,
+            final int readBufferSize) {
+
+      this.stage = stage;
+      this.addressOfHost = addressOfHost;
+      this.consumerOfUnknownResponses = consumerOfUnknownResponses;
+      this.probeInterval = probeInterval;
+      this.writeBufferSize = writeBufferSize;
+      this.readBufferPoolSize = readBufferPoolSize;
+      this.readBufferSize = readBufferSize;
+    }
+  }
+
+  public static interface ClientConsumer extends ResponseChannelConsumer, Scheduled, Stoppable {
+    Completes<Response> requestWith(final Request request);
+  }
+
+  public static class ClientRequesterConsumerActor extends Actor implements ClientConsumer {
+    private final ByteBuffer buffer;
+    private final Map<String, Completes<Response>> completables;
+    private final ClientRequestResponseChannel channel;
+    private final Configuration configuration;
+    private ResponseParser parser;
+    private final Cancellable probe;
+
+    public ClientRequesterConsumerActor(final Configuration configuration) throws Exception {
+      this.configuration = configuration;
+      this.buffer = ByteBufferAllocator.allocate(configuration.writeBufferSize);
+      this.completables = new HashMap<>();
+      this.channel = clientChannel(configuration);
+      this.probe = stage().scheduler().schedule(selfAs(Scheduled.class), null, 1, configuration.probeInterval);
+    }
+
+    @Override
+    public void consume(final ConsumerByteBuffer buffer) {
+      if (parser == null) {
+        parser = ResponseParser.parserFor(buffer.asByteBuffer());
+      } else {
+        parser.parseNext(buffer.asByteBuffer());
+      }
+      buffer.release();
+
+      while (parser.hasFullResponse()) {
+        final Response response = parser.fullResponse();
+        final ResponseHeader correlationId = response.headers.headerOf(ResponseHeader.XCorrelationID);
+        if (correlationId == null) {
+          logger().log("Client Consumer: Cannot complete response because no correlation id.");
+          configuration.consumerOfUnknownResponses.consume(response);
+        } else {
+          final Completes<Response> completes = completables.remove(correlationId.value);
+          if (completes == null) {
+            configuration.stage.world().defaultLogger().log(
+                    "Client Consumer: Cannot complete response because mismatched correlation id: " +
+                     correlationId.name);
+            configuration.consumerOfUnknownResponses.consume(response);
+          } else {
+            completes.with(response);
+          }
+        }
+      }
+    }
+
+    @Override
+    public void intervalSignal(final Scheduled scheduled, final Object data) {
+      channel.probeChannel();
+    }
+
+    public Completes<Response> requestWith(final Request request) {
+      RequestHeader correlationId = request.headers.headerOf(RequestHeader.XCorrelationID);
+      
+      final Request readyRequest;
+      
+      if (correlationId == null) {
+        correlationId = RequestHeader.of(RequestHeader.XCorrelationID, UUID.randomUUID().toString());
+        readyRequest = request.and(correlationId);
+      } else {
+        readyRequest = request;
+      }
+
+      completables.put(correlationId.value, completes());
+
+      buffer.clear();
+      buffer.put(Converters.textToBytes(readyRequest.toString()));
+      buffer.flip();
+      channel.requestWith(buffer);
+
+      return completes();
+    }
+
+    @Override
+    public void stop() {
+      channel.close();
+      probe.cancel();
+    }
+
+    private ClientRequestResponseChannel clientChannel(final Configuration configuration) throws Exception {
+      return new ClientRequestResponseChannel(
+              configuration.addressOfHost,
+              selfAs(ResponseChannelConsumer.class),
+              configuration.readBufferPoolSize,
+              configuration.readBufferSize,
+              logger());
+    }
+  }
+}
---
+++ src/test/java/io/vlingo/http/resource/ClientTest.java
@@ -0,0 +1,96 @@
+package io.vlingo.http.resource;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URI;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.vlingo.actors.testkit.TestUntil;
+import io.vlingo.http.Body;
+import io.vlingo.http.Method;
+import io.vlingo.http.Request;
+import io.vlingo.http.RequestHeader;
+import io.vlingo.http.Response;
+import io.vlingo.http.ResponseHeader;
+import io.vlingo.http.resource.Client.Configuration;
+import io.vlingo.http.resource.Server.Sizing;
+import io.vlingo.http.resource.Server.Timing;
+import io.vlingo.http.sample.user.model.User;
+
+public class ClientTest extends ResourceTestFixtures {
+  private Client client;
+  private int expectedHeaderCount;
+  private Response expectedResponse;
+  private ResponseHeader location;
+  private Server server;
+  private AtomicInteger unknownResponseCount = new AtomicInteger(0);
+
+  @Test public void testNada() {}
+
+  @Test
+  public void testThatClientRequestsAndReceivesResponse() throws Exception {
+    final TestUntil until = TestUntil.happenings(1);
+
+    client = Client.using(Configuration.defaultedExceptFor(world.stage(), new UnknownResponseConsumer(until)));
+
+    client.requestWith(
+            Request
+              .has(Method.POST)
+              .and(URI.create("/users"))
+              .and(RequestHeader.host("vlingo.io"))
+              .and(RequestHeader.contentLength(johnDoeUserSerialized.length()))
+              .and(Body.from(johnDoeUserSerialized)))
+          .after(response -> expectedResponse = response)
+          .andThen(response -> expectedHeaderCount = response.headers.size())
+          .andThen(response -> location = response.headers.headerOf(ResponseHeader.Location))
+          .atLast(response -> until.completeNow());
+
+    until.completes();
+
+    assertNotNull(expectedResponse);
+    assertEquals(Response.Created, expectedResponse.status);
+    assertEquals(3, expectedHeaderCount);
+    assertNotNull(location);
+    assertEquals(0, unknownResponseCount.get());
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    super.setUp();
+
+    User.resetId();
+
+    server = Server.startWith(world.stage(), resources, 8080, new Sizing(10, 100, 10240), new Timing(1, 2, 100));
+
+    Thread.sleep(10); // delay for server startup
+  }
+
+  @After
+  public void tearDown() {
+    if (client != null) client.close();
+    
+    if (server != null) server.stop();
+    
+    super.tearDown();
+  }
+
+  private class UnknownResponseConsumer implements ResponseConsumer {
+    private final TestUntil until;
+    
+    UnknownResponseConsumer(final TestUntil until) {
+      this.until = until;
+    }
+
+    @Override
+    public void consume(final Response response) {
+      unknownResponseCount.incrementAndGet();
+      System.out.println("UNKNOWN RESPONSE:\n" + response);
+      until.completeNow();
+    }
+  }
+}
