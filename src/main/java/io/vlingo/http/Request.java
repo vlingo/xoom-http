58fc741d0caa448b8e2ff12c525abcb83e1a44f7
Vaughn Vernon <vaughn@forcomprehension.com>
2018-07-04 11:06:18 -0700

    Implemented fluent APIs.

--- src/main/java/io/vlingo/http/Request.java
+++ src/main/java/io/vlingo/http/Request.java
@@ -25,6 +25,68 @@ public class Request {
     return RequestParser.parserFor(requestContent).fullRequest();
   }
 
+  // ===========================================
+  // fluent API follows
+  // ===========================================
+
+  public static Request has(final Method method) {
+    return new Request(method);
+  }
+
+  public Request and(final Body body) {
+    return new Request(this.method, this.uri, this.version, this.headers, body);
+  }
+
+  public Request and(final RequestHeader header) {
+    final Headers<RequestHeader> headers = Headers.empty();
+    return new Request(this.method, this.uri, this.version, headers.and(this.headers).and(header), this.body);
+  }
+
+  public Request and(final Headers<RequestHeader> headers) {
+    return new Request(this.method, this.uri, this.version, headers, this.body);
+  }
+
+  public Request and(final URI uri) {
+    return new Request(this.method, uri, this.version, this.headers, this.body);
+  }
+
+  public Request and(final Version version) {
+    return new Request(this.method, this.uri, version, this.headers, this.body);
+  }
+
+  // ===========================================
+  // less fluent API follows
+  // ===========================================
+
+  public static Request method(final Method method) {
+    return new Request(method);
+  }
+
+  public Request body(final String body) {
+    return new Request(this.method, this.uri, this.version, this.headers, Body.from(body));
+  }
+
+  public Request header(final String name, final String value) {
+    final Headers<RequestHeader> headers = Headers.empty();
+    return new Request(this.method, this.uri, this.version, headers.and(this.headers).and(RequestHeader.of(name, value)), this.body);
+  }
+
+  public Request header(final String name, final int value) {
+    return header(name, String.valueOf(value));
+  }
+
+  public Request uri(final String uri) {
+    return new Request(this.method, URI.create(uri), this.version, this.headers, this.body);
+  }
+
+  public Request version(final String version) {
+    return new Request(this.method, this.uri, Version.from(version), this.headers, this.body);
+  }
+
+  // ===========================================
+  // instance
+  // ===========================================
+
   public Header headerOf(final String name) {
     for (final Header header : headers) {
       if (header.name.equals(name)) {
@@ -45,5 +107,9 @@ public class Request {
     this.version = version;
     this.headers = headers;
     this.body = body;
+  }
+
+  private Request(final Method method) {
+    this(method, URI.create("/"), Version.Http1_1, Headers.empty(), Body.from(""));
   }
 }
