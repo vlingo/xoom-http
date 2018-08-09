4879e66fc0857115b0caa831ddb98f65791d6e42
Aleix Morgadas <aleix602@gmail.com>
2018-08-09 20:35:32 +0200

    updated editorconfig

--- src/test/java/io/vlingo/http/resource/ConfigurationTest.java
+++ src/test/java/io/vlingo/http/resource/ConfigurationTest.java
@@ -2,26 +2,27 @@ package io.vlingo.http.resource;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ConfigurationTest {
 
-    @Test
-    public void testThatConfigurationDefaults() {
-        final Configuration configuration =
-                Configuration.define();
+  @Test
+  public void testThatConfigurationDefaults() {
+    final Configuration configuration =
+      Configuration.define();
 
-        assertNotNull(configuration);
-        assertEquals(8080, configuration.port());
+    assertNotNull(configuration);
+    assertEquals(8080, configuration.port());
 
-        assertNotNull(configuration.sizing());
-        assertEquals(10, configuration.sizing().dispatcherPoolSize);
-        assertEquals(100, configuration.sizing().maxBufferPoolSize);
-        assertEquals(65535, configuration.sizing().maxMessageSize);
+    assertNotNull(configuration.sizing());
+    assertEquals(10, configuration.sizing().dispatcherPoolSize);
+    assertEquals(100, configuration.sizing().maxBufferPoolSize);
+    assertEquals(65535, configuration.sizing().maxMessageSize);
 
-        assertNotNull(configuration.timing());
-        assertEquals(10, configuration.timing().probeInterval);
-        assertEquals(10, configuration.timing().probeTimeout);
-        assertEquals(100, configuration.timing().requestMissingContentTimeout);
-    }
+    assertNotNull(configuration.timing());
+    assertEquals(10, configuration.timing().probeInterval);
+    assertEquals(10, configuration.timing().probeTimeout);
+    assertEquals(100, configuration.timing().requestMissingContentTimeout);
+  }
 }
