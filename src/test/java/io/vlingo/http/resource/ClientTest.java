0f4eb26b1eeb67bfb7d24984ddb9d256f11bb101
Vaughn Vernon <vaughn@forcomprehension.com>
2018-08-25 14:53:54 -0700

    Test with new Sizing attributes.

--- src/test/java/io/vlingo/http/resource/ClientTest.java
+++ src/test/java/io/vlingo/http/resource/ClientTest.java
@@ -71,7 +71,7 @@ public class ClientTest extends ResourceTestFixtures {
 
     User.resetId();
 
-    server = Server.startWith(world.stage(), resources, 8080, new Sizing(10, 100, 10240), new Timing(1, 2, 100));
+    server = Server.startWith(world.stage(), resources, 8080, new Sizing(1, 10, 100, 10240), new Timing(1, 2, 100));
 
     Thread.sleep(10); // delay for server startup
   }
