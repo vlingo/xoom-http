3e4274fa4acec815df7765c8eb544674d020142e
Aleix Morgadas <aleix602@gmail.com>
2018-09-01 20:48:33 +0200

    created ParameterResolver

---
+++ src/main/java/io/vlingo/http/resource/ParameterResolver.java
@@ -0,0 +1,74 @@
+package io.vlingo.http.resource;
+
+import io.vlingo.http.Header;
+import io.vlingo.http.Request;
+
+import java.util.function.BiFunction;
+
+class ParameterResolver<T> {
+  public final Type type;
+  public final Class<T> paramClass;
+  private final BiFunction<Request, Action.MappedParameters, T> resolver;
+
+  private ParameterResolver(final Type type, final Class<T> paramClass, final BiFunction<Request, Action.MappedParameters, T> resolver) {
+    this.type = type;
+    this.paramClass = paramClass;
+    this.resolver = resolver;
+  }
+
+  @SuppressWarnings("unchecked")
+  public static <T> ParameterResolver<T> path(final int position, final Class<T> paramClass) {
+    return new ParameterResolver<>(Type.PATH, paramClass, (request, mappedParameters) -> {
+      Object value = mappedParameters.mapped.get(position).value;
+      if (paramClass.isInstance(value)) {
+        return (T) value;
+      }
+      throw new IllegalArgumentException("Value " + value + " is of type " + mappedParameters.mapped.get(0).type + " instead of " + paramClass.getSimpleName());
+    });
+  }
+
+  public static <T> ParameterResolver<T> body(final Class<T> bodyClass) {
+      return body(bodyClass, DefaultMapper.instance);
+  }
+
+  public static <T> ParameterResolver<T> body(final Class<T> bodyClass, final Mapper mapper) {
+    return new ParameterResolver<>(Type.BODY, bodyClass, ((request, mappedParameters) ->
+      mapper.from(request.body.toString(), bodyClass)));
+  }
+
+  public static ParameterResolver<Header> header(String headerName) {
+    return new ParameterResolver<>(Type.HEADER, Header.class, ((request, mappedParameters) ->
+      request.headerOf(headerName)));
+  }
+
+  public static ParameterResolver<String> query(String name) {
+    return new ParameterResolver<>(Type.QUERY, String.class, (((request, mappedParameters) ->
+      request.queryParameters().valuesOf(name).get(0))));
+  }
+
+  public static <T> ParameterResolver<T> query(String name, Class<T> type) {
+    return new ParameterResolver<>(Type.QUERY, type, (((request, mappedParameters) -> {
+      if (type == Integer.class) {
+        return type.cast(Integer.valueOf(request.queryParameters().valuesOf(name).get(0)));
+      } else if (type == String.class) {
+        return type.cast(request.queryParameters().valuesOf(name).get(0));
+      } else if (type == Float.class) {
+        return type.cast(Float.valueOf(request.queryParameters().valuesOf(name).get(0)));
+      } else if (type == Long.class) {
+        return type.cast(Integer.valueOf(request.queryParameters().valuesOf(name).get(0)));
+      }
+      throw new IllegalArgumentException("unknown type " + type.getSimpleName());
+    })));
+  }
+
+  public T apply(Request request, Action.MappedParameters mappedParameters) {
+    return resolver.apply(request, mappedParameters);
+  }
+
+  enum Type {
+    PATH,
+    BODY,
+    HEADER,
+    QUERY
+  }
+}
---
+++ src/test/java/io/vlingo/http/resource/ParameterResolverTest.java
@@ -0,0 +1,81 @@
+package io.vlingo.http.resource;
+
+import io.vlingo.http.*;
+import io.vlingo.http.sample.user.NameData;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URI;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+public class ParameterResolverTest {
+  private Request request;
+  private Action.MappedParameters mappedParameters;
+
+  @Before
+  public void setUp() {
+    request = Request.has(Method.POST)
+      .and(Version.Http1_1)
+      .and(URI.create("/user/my-post?page=10"))
+      .and(RequestHeader.from("Host:www.vlingo.io"))
+      .and(Body.from("{\"given\":\"John\",\"family\":\"Doe\"}"));
+
+    mappedParameters = new Action.MappedParameters(1, Method.GET, "ignored", Collections.singletonList(
+        new Action.MappedParameter("String", "my-post"))
+      );
+  }
+
+  @Test
+  public void path() {
+    final ParameterResolver<String> resolver = ParameterResolver.path(0, String.class);
+
+    String result = resolver.apply(request, mappedParameters);
+
+    assertEquals("my-post", result);
+    assertEquals(ParameterResolver.Type.PATH, resolver.type);
+  }
+
+  @Test
+  public void body() {
+    final ParameterResolver<NameData> resolver = ParameterResolver.body(NameData.class);
+
+    final NameData result = resolver.apply(request, mappedParameters);
+    final NameData expected = new NameData("John", "Doe");
+
+    assertEquals(expected.toString(), result.toString());
+    assertEquals(ParameterResolver.Type.BODY, resolver.type);
+  }
+
+  @Test
+  public void header() {
+    final ParameterResolver<Header> resolver = ParameterResolver.header("Host");
+
+    final Header result = resolver.apply(request, mappedParameters);
+
+    assertEquals("Host", result.name);
+    assertEquals("www.vlingo.io", result.value);
+    assertEquals(ParameterResolver.Type.HEADER, resolver.type);
+  }
+
+  @Test
+  public void query() {
+    final ParameterResolver<String> resolver = ParameterResolver.query("page");
+
+    final String result = resolver.apply(request, mappedParameters);
+
+    assertEquals("10", result);
+    assertEquals(ParameterResolver.Type.QUERY, resolver.type);
+  }
+
+  @Test
+  public void queryWithType() {
+    final ParameterResolver<Integer> resolver = ParameterResolver.query("page", Integer.class);
+
+    final Integer result = resolver.apply(request, mappedParameters);
+
+    assertEquals(10, (int) result);
+    assertEquals(ParameterResolver.Type.QUERY, resolver.type);
+  }
+}
