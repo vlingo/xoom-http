ab5b7e2d55fc5c86df2796aab9ce339f5097ff32
VaughnVernon <vaughn@forcomprehension.com>
2019-04-21 21:47:49 -0700

    Test with various types of static files.

---
+++ src/test/resources/content/js/vuetify.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: "#00796B",
+    secondary: "#B2DFDB",
+    accent: "#009688",
+    error: "#f44336",
+    warning: "#ffeb3b",
+    info: "#2196f3",
+    success: "#4caf50"
+  },
+  iconfont: 'md',
+})
