21ddb699c217fec139ce4fcd253261d1d15b44ba
greg <greg@colorings.us>
2019-07-15 08:12:11 -0600

    replace TestUntil with AccessSafely in MockCompletesEventuallyResponse

--- src/test/java/io/vlingo/http/resource/MockCompletesEventuallyResponse.java
+++ src/test/java/io/vlingo/http/resource/MockCompletesEventuallyResponse.java
@@ -9,14 +9,29 @@ package io.vlingo.http.resource;
 
 import io.vlingo.actors.Address;
 import io.vlingo.actors.CompletesEventually;
-import io.vlingo.actors.testkit.TestUntil;
+import io.vlingo.actors.testkit.AccessSafely;
 import io.vlingo.http.Response;
 
 public class MockCompletesEventuallyResponse implements CompletesEventually {
-  public static TestUntil untilWith;
+  private AccessSafely withCalls = AccessSafely.afterCompleting(0);
 
   public Response response;
-  
+
+  /**
+   * Answer with an AccessSafely which writes nulls to "with" and reads the write count from the "completed".
+   * <p>
+   * Note: Clients can replace the default lambdas with their own via readingWith/writingWith.
+   * 
+   * @param n Number of times with(outcome) must be called before readFrom(...) will return.
+   * @return
+   */
+  public AccessSafely expectWithTimes(int n) {
+    withCalls = AccessSafely.afterCompleting(n)
+        .writingWith("with", (x) -> {})
+        .readingWith("completed", () -> withCalls.totalWrites());
+    return withCalls;
+  }
+
   @Override
   public Address address() {
     return null;
@@ -25,6 +40,6 @@ public class MockCompletesEventuallyResponse implements CompletesEventually {
   @Override
   public void with(final Object outcome) {
     this.response = (Response) outcome;
-    if (untilWith != null) untilWith.happened();
+    withCalls.writeUsing("with", null);
   }
 }
