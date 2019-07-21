fc10e3ec2528d46eda6968377ad4f1e702a86f48
VaughnVernon <vaughn@forcomprehension.com>
2019-07-20 23:06:18 -0600

    Support HTTP 1.0.

--- src/main/java/io/vlingo/http/Version.java
+++ src/main/java/io/vlingo/http/Version.java
@@ -8,6 +8,9 @@
 package io.vlingo.http;
 
 public enum Version {
+  Http1_0 {
+    @Override public boolean isHttp1_0() { return true; }
+  },
   Http1_1 {
     @Override public boolean isHttp1_1() { return true; }
   },
@@ -16,11 +19,14 @@ public enum Version {
     @Override public boolean isHttp2_0() { return true; }
   };
 
+  public static final String HTTP_1_0 = "HTTP/1.0";
   public static final String HTTP_1_1 = "HTTP/1.1";
   public static final String HTTP_2_0 = "HTTP/2.0";
-  
+
   public static Version from(final String version) {
-    if (version.equals(HTTP_1_1)) {
+    if (version.equals(HTTP_1_0)) {
+      return Http1_0;
+    } else if (version.equals(HTTP_1_1)) {
       return Http1_1;
     } else if (version.equals(HTTP_2_0)) {
       return Http2_0;
@@ -28,12 +34,15 @@ public enum Version {
     throw new IllegalArgumentException("Unsupported HTTP/version: " + version);
   }
 
+  public boolean isHttp1_0() { return false; }
   public boolean isHttp1_1() { return false; }
   public boolean isHttp2_0() { return false; }
-  
+
   @Override
   public String toString() {
-    if (this.isHttp1_1()) {
+    if (this.isHttp1_0()) {
+      return HTTP_1_0;
+    } else if (this.isHttp1_1()) {
       return HTTP_1_1;
     } else if (this.isHttp2_0()) {
       return HTTP_2_0;
