dd17fc90d0b3bb5569d5920c2d28ccb15702f1ec
VaughnVernon <vaughn@forcomprehension.com>
2019-07-20 22:06:35 -0600

    Organized imports. Copyright.

--- src/main/java/io/vlingo/http/resource/RequestExecutor.java
+++ src/main/java/io/vlingo/http/resource/RequestExecutor.java
@@ -1,10 +1,17 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 package io.vlingo.http.resource;
+
+import java.util.function.Supplier;
 
 import io.vlingo.actors.Logger;
 import io.vlingo.common.Completes;
 import io.vlingo.http.Response;
-
-import java.util.function.Supplier;
 
 abstract class RequestExecutor {
 
