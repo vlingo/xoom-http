a43c003c638c99c55087bbfb0ae38044b8eac622
VaughnVernon <vaughn@forcomprehension.com>
2019-07-23 00:02:43 -0700

    Fixed header tests and added chunk test.

--- src/test/java/io/vlingo/http/ResponseTest.java
+++ src/test/java/io/vlingo/http/ResponseTest.java
@@ -21,9 +21,9 @@ public class ResponseTest {
   @Test
   public void testResponseWithOneHeaderNoEntity() {
     final Response response = Response.of(Version.Http1_1, Ok, headers(CacheControl, "max-age=3600"));
-    
-    final String facsimile = "HTTP/1.1 200 OK\nCache-Control: max-age=3600\nContent-Length: 0\n\n";
-    
+
+    final String facsimile = "HTTP/1.1 200 OK\nCache-Control: max-age=3600\n\n";
+
     assertEquals(facsimile, response.toString());
   }
 
@@ -31,18 +31,18 @@ public class ResponseTest {
   public void testResponseWithOneHeaderAndEntity() {
     final String body = "{ text : \"some text\" }";
     final Response response = Response.of(Version.Http1_1, Ok, headers(CacheControl, "max-age=3600"), body);
-    
+
     final String facsimile = "HTTP/1.1 200 OK\nCache-Control: max-age=3600\nContent-Length: " + body.length() + "\n\n{ text : \"some text\" }";
-    
+
     assertEquals(facsimile, response.toString());
   }
 
   @Test
   public void testResponseWithMultipleHeadersNoEntity() {
     final Response response = Response.of(Version.Http1_1, Ok, headers(of(ETag, "123ABC")).and(of(CacheControl, "max-age=3600")));
-    
-    final String facsimile = "HTTP/1.1 200 OK\nETag: 123ABC\nCache-Control: max-age=3600\nContent-Length: 0\n\n";
-    
+
+    final String facsimile = "HTTP/1.1 200 OK\nETag: 123ABC\nCache-Control: max-age=3600\n\n";
+
     assertEquals(facsimile, response.toString());
   }
 
@@ -50,9 +50,29 @@ public class ResponseTest {
   public void testResponseWithMultipleHeadersAndEntity() {
     final String body = "{ text : \"some text\" }";
     final Response response = Response.of(Version.Http1_1, Ok, headers(of(ETag, "123ABC")).and(of(CacheControl, "max-age=3600")), body);
-    
+
     final String facsimile = "HTTP/1.1 200 OK\nETag: 123ABC\nCache-Control: max-age=3600\nContent-Length: " + body.length() + "\n\n{ text : \"some text\" }";
-    
+
     assertEquals(facsimile, response.toString());
   }
+
+  @Test
+  public void testThatChunkedResponseIsValid() {
+    final String Chunk1 = "ABCDEFGHIJKLMNOPQRSTUVWYYZ0123";
+    final String Chunk2 = "abcdefghijklmnopqrstuvwxyz012345";
+
+    final String chunks = Integer.toHexString(Chunk1.length()) + "\r\n" + Chunk1 + "\r\n" + Integer.toHexString(Chunk2.length()) + "\r\n" + Chunk2 + "\r\n0\r\n";
+    final String responseMultiHeadersWithChunkedBody =
+            "HTTP/1.1 200 OK\nTransfer-Encoding: chunked\nCache-Control: no-cache\n\n" + chunks;
+
+    final Response response =
+            Response.of(
+                    Version.Http1_1,
+                    Ok,
+                    headers(of(ResponseHeader.TransferEncoding, "chunked")).and(of(CacheControl, "no-cache")),
+                    Body.beginChunked().appendChunk(Chunk1).appendChunk(Chunk2).end());
+
+    assertEquals(responseMultiHeadersWithChunkedBody, response.toString());
+  }
 }
+
