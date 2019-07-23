2c74d0bf7c7478fe38260c7f8b84cec662b581b0
VaughnVernon <vaughn@forcomprehension.com>
2019-07-23 00:09:25 -0700

    Initial revision of ChunkedBodyTest.

---
+++ src/test/java/io/vlingo/http/ChunkedBodyTest.java
@@ -0,0 +1,34 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.http;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ChunkedBodyTest {
+  private static final String Chunk1 = "ABCDEFGHIJKLMNOPQRSTUVWYYZ0123";
+  private static final String Chunk2 = "abcdefghijklmnopqrstuvwxyz012345";
+
+  @Test
+  public void testThatChunkedBodyChunks() {
+    final ChunkedBody body =
+            Body
+              .beginChunked()
+              .appendChunk(Chunk1)
+              .appendChunk(Chunk2)
+              .end();
+
+    assertTrue(body.content().contains(asChunk(Chunk1)));
+    assertTrue(body.content().contains(asChunk(Chunk2)));
+  }
+
+  private String asChunk(final String content) {
+    return Integer.toHexString(content.length()) + "\r\n" + content + "\r\n";
+  }
+}
