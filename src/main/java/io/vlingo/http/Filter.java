10dfc79587e95c46c90dc52f673182514811ebbb
VaughnVernon <vaughn@forcomprehension.com>
2019-08-22 22:48:24 -0700

    Support Filters.

---
+++ src/main/java/io/vlingo/http/Filter.java
@@ -0,0 +1,23 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.http;
+
+/**
+ * Base filter supporting starting and stopping.
+ */
+public abstract class Filter {
+  /**
+   * Construct my state.
+   */
+  protected Filter() { }
+
+  /**
+   * Sent when I am to be stopped.
+   */
+  public abstract void stop();
+}
