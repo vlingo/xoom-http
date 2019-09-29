0f7fb7a631f08db05a8f1eb26ff40361df59fc8b
VaughnVernon <vaughn@forcomprehension.com>
2019-09-29 00:00:03 -0700

    Handle through RequestHander.

--- src/main/java/io/vlingo/http/resource/ResourceRequestHandler__Proxy.java
+++ src/main/java/io/vlingo/http/resource/ResourceRequestHandler__Proxy.java
@@ -13,10 +13,13 @@ import io.vlingo.actors.Actor;
 import io.vlingo.actors.DeadLetter;
 import io.vlingo.actors.LocalMessage;
 import io.vlingo.actors.Mailbox;
+import io.vlingo.http.Context;
+import io.vlingo.http.resource.Action.MappedParameters;
 
 public class ResourceRequestHandler__Proxy implements ResourceRequestHandler {
 
   private static final String handleForRepresentation1 = "handleFor(io.vlingo.http.Context, java.util.function.Consumer)";
+  private static final String handleForRepresentation2 = "handleFor(io.vlingo.http.Context, io.vlingo.http.Action.MappedParameters, io.vlingo.http.RequestHandler)";
 
   private final Actor actor;
   private final Mailbox mailbox;
@@ -26,6 +29,7 @@ public class ResourceRequestHandler__Proxy implements ResourceRequestHandler {
     this.mailbox = mailbox;
   }
 
+  @Override
   @SuppressWarnings("rawtypes")
   public void handleFor(io.vlingo.http.Context arg0, java.util.function.Consumer arg1) {
     if (!actor.isStopped()) {
@@ -34,6 +38,17 @@ public class ResourceRequestHandler__Proxy implements ResourceRequestHandler {
       else { mailbox.send(new LocalMessage<ResourceRequestHandler>(actor, ResourceRequestHandler.class, consumer, handleForRepresentation1)); }
     } else {
       actor.deadLetters().failedDelivery(new DeadLetter(actor, handleForRepresentation1));
+    }
+  }
+
+  @Override
+  public void handleFor(final Context arg0, final MappedParameters arg1, final RequestHandler arg2) {
+    if (!actor.isStopped()) {
+      final Consumer<ResourceRequestHandler> consumer = (actor) -> actor.handleFor(arg0, arg1, arg2);
+      if (mailbox.isPreallocated()) { mailbox.send(actor, ResourceRequestHandler.class, consumer, null, handleForRepresentation2); }
+      else { mailbox.send(new LocalMessage<ResourceRequestHandler>(actor, ResourceRequestHandler.class, consumer, handleForRepresentation2)); }
+    } else {
+      actor.deadLetters().failedDelivery(new DeadLetter(actor, handleForRepresentation2));
     }
   }
 }
