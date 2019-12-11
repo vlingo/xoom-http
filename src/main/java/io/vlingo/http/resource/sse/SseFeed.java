11c712415a08f9a05dae2d8c5eb51f67339a0734
VaughnVernon <vaughn@forcomprehension.com>
2019-12-10 23:14:33 -0700

    Use ActorInstantiator<A>.

--- src/main/java/io/vlingo/http/resource/sse/SseFeed.java
+++ src/main/java/io/vlingo/http/resource/sse/SseFeed.java
@@ -9,6 +9,32 @@ package io.vlingo.http.resource.sse;
 
 import java.util.Collection;
 
+import io.vlingo.actors.Actor;
+import io.vlingo.actors.ActorInstantiator;
+
 public interface SseFeed {
   void to(final Collection<SseSubscriber> subscribers);
+
+  static abstract class SseFeedInstantiator<A extends Actor> implements ActorInstantiator<A> {
+    protected final Class<A> type;
+    protected final String streamName;
+    protected final int feedPayload;
+    protected final String feedDefaultId;
+
+    public SseFeedInstantiator(
+            final Class<A> type,
+            final String streamName,
+            final int feedPayload,
+            final String feedDefaultId) {
+      this.type = type;
+      this.streamName = streamName;
+      this.feedPayload = feedPayload;
+      this.feedDefaultId = feedDefaultId;
+    }
+
+    @Override
+    public Class<A> type() {
+      return type;
+    }
+  }
 }
