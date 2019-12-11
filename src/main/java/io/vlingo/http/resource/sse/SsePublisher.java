9c8668648c4fa9e091a077b2bf25cf404540c45b
VaughnVernon <vaughn@forcomprehension.com>
2019-12-10 23:15:08 -0700

    Use ActorInstantiator<A>.

--- src/main/java/io/vlingo/http/resource/sse/SsePublisher.java
+++ src/main/java/io/vlingo/http/resource/sse/SsePublisher.java
@@ -7,9 +7,38 @@
 
 package io.vlingo.http.resource.sse;
 
+import io.vlingo.actors.Actor;
+import io.vlingo.actors.ActorInstantiator;
 import io.vlingo.actors.Stoppable;
+import io.vlingo.http.resource.sse.SseStreamResource.SsePublisherActor;
 
 public interface SsePublisher extends Stoppable {
   void subscribe(final SseSubscriber subscriber);
   void unsubscribe(final SseSubscriber subscriber);
+
+  static class SsePublisherInstantiator implements ActorInstantiator<SsePublisherActor> {
+    private final String streamName;
+    private final Class<? extends Actor> feedClass;
+    private final int feedPayload;
+    private final int feedInterval;
+    private final String feedDefaultId;
+
+    public SsePublisherInstantiator(final String streamName, final Class<? extends Actor> feedClass, final int feedPayload, final int feedInterval, final String feedDefaultId) {
+      this.streamName = streamName;
+      this.feedClass = feedClass;
+      this.feedPayload = feedPayload;
+      this.feedInterval = feedInterval;
+      this.feedDefaultId = feedDefaultId;
+    }
+
+    @Override
+    public SsePublisherActor instantiate() {
+      return new SsePublisherActor(streamName, feedClass, feedPayload, feedInterval, feedDefaultId);
+    }
+
+    @Override
+    public Class<SsePublisherActor> type() {
+      return SsePublisherActor.class;
+    }
+  }
 }
