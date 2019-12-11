c492192dfbb804f4e86ffe0f3695910982287614
VaughnVernon <vaughn@forcomprehension.com>
2019-12-11 13:58:48 -0700

    Added testThatFeedWithInstantiatorFeeds().

--- src/test/java/io/vlingo/http/resource/sse/SseStreamResourceTest.java
+++ src/test/java/io/vlingo/http/resource/sse/SseStreamResourceTest.java
@@ -13,15 +13,21 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
+import io.vlingo.actors.ActorInstantiator;
+import io.vlingo.actors.ActorInstantiator.Registry;
 import io.vlingo.actors.World;
 import io.vlingo.actors.testkit.AccessSafely;
+import io.vlingo.http.Context;
 import io.vlingo.http.Method;
 import io.vlingo.http.Request;
 import io.vlingo.http.RequestHeader;
 import io.vlingo.http.resource.Configuration;
+import io.vlingo.http.resource.MockCompletesEventuallyResponse;
 import io.vlingo.http.sample.user.AllSseFeedActor;
 
 public class SseStreamResourceTest {
+  private SseClient client;
+  private MockRequestResponseContext context;
   private MockSseStreamResource resource;
   private World world;
 
@@ -39,7 +45,7 @@ public class SseStreamResourceTest {
     resource.nextRequest(request);
 
     resource.subscribeToStream("all", AllSseFeedActor.class, 10, 10, "1");
-    
+
     assertEquals(10, (int) respondWithSafely.readFrom("count"));
 
     assertEquals(10, resource.requestResponseContext.channel.respondWithCount.get());
@@ -59,7 +65,7 @@ public class SseStreamResourceTest {
     resource.nextRequest(subscribe);
 
     resource.subscribeToStream("all", AllSseFeedActor.class, 1, 10, "1");
-    
+
     assertTrue(1 <= (int) respondWithSafely.readFrom("count"));
     assertTrue(1 <= resource.requestResponseContext.channel.respondWithCount.get());
 
@@ -77,9 +83,45 @@ public class SseStreamResourceTest {
     resource.nextRequest(unsubscribe);
 
     resource.unsubscribeFromStream("all", clientId);
-    
+
     assertEquals(1, (int) abandonSafely.readFrom("count"));
     assertEquals(1, resource.requestResponseContext.channel.abandonCount.get());
+  }
+
+  @Test
+  public void testThatFeedWithInstantiatorFeeds() {
+    final MockCompletesEventuallyResponse completes = new MockCompletesEventuallyResponse();
+    final AccessSafely respondWithSafely = context.channel.expectRespondWith(1);
+
+    // NOTE: Any reference to AllSseFeedActor automatically
+    // causes the AllSseFeedInstantiator to be registered
+
+    // somehow the static initializer doesn't work for (this) JUnit tests
+    @SuppressWarnings({ "unused", "rawtypes" })
+    final Class c = AllSseFeedActor.class;  // not registered
+    AllSseFeedActor.registerInstantiator(); // registered
+
+    final Request request =
+            Request
+              .method(Method.GET)
+              .uri("/eventstreams/all")
+              .and(RequestHeader.host("StreamsRUs.co"))
+              .and(RequestHeader.accept("text/event-stream"));
+
+    final SseStreamResource sseStreamResource = new SseStreamResource(world);
+    final Context requestContext = new Context(context, request, completes);
+    sseStreamResource.__internal__test_set_up(requestContext, world.stage());
+
+    final ActorInstantiator<?> instantiator = Registry.instantiatorFor(AllSseFeedActor.class);
+    instantiator.set("feedClass", AllSseFeedActor.class);
+    instantiator.set("streamName", "all");
+    instantiator.set("feedPayload", 1);
+    instantiator.set("feedDefaultId", "123");
+
+    sseStreamResource.subscribeToStream("all", AllSseFeedActor.class, 1, 100, "123");
+
+    final int completedCount = respondWithSafely.readFrom("count");
+    assertEquals(1, completedCount);
   }
 
   @Before
@@ -87,5 +129,8 @@ public class SseStreamResourceTest {
     world = World.startWithDefaults("test-stream-userResource");
     Configuration.define();
     resource = new MockSseStreamResource(world);
+    Configuration.define();
+    context = new MockRequestResponseContext(new MockResponseSenderChannel());
+    client = new SseClient(context);
   }
 }
