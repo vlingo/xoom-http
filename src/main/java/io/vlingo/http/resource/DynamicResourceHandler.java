6d39b9df050483fe237563b6b903b44fe8d52498
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-11-05 10:32:32 -0300

    Added DynamicResourceHandler

---
+++ src/main/java/io/vlingo/http/resource/DynamicResourceHandler.java
@@ -0,0 +1,31 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+package io.vlingo.http.resource;
+
+import io.vlingo.actors.Logger;
+import io.vlingo.actors.Stage;
+
+public abstract class DynamicResourceHandler {
+  private final Logger logger;
+  private final Stage stage;
+
+  protected DynamicResourceHandler(final Stage stage) {
+    this.stage = stage;
+    this.logger = stage.world().defaultLogger();
+  }
+
+  public abstract Resource<?> routes();
+
+  protected Logger logger() {
+    return logger;
+  }
+
+  protected Stage stage() {
+    return stage;
+  }
+
+}
