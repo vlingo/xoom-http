1009a549da4ba0895265107861287deb3c6b0cee
Brian Wehrle <bwolesa@gmail.com>
2021-04-08 23:37:48 +0200

    Add DefaultPlainTextMapper; not enabled by default
    Signed-off-by: Brian Wehrle <bwolesa@gmail.com>

---
+++ src/main/java/io/vlingo/http/resource/DefaultTextPlainMapper.java
@@ -0,0 +1,26 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.http.resource;
+
+public class DefaultTextPlainMapper implements Mapper {
+  public static final Mapper instance = new DefaultTextPlainMapper();
+  
+  @Override
+  @SuppressWarnings("unchecked")
+  public <T> T from(final String data, final Class<T> type) {
+    if (type.getName().equals("java.lang.String")) {
+      return (T) data;
+    }
+    throw new IllegalArgumentException("Cannot deserialize text into type");
+  }
+
+  @Override
+  public <T> String from(final T data) {
+    return data.toString();
+  }
+}
