03431d2429f712b48cbeaa49683cc9bb58cf7183
VaughnVernon <vaughn@kalele.io>
2021-09-06 12:10:15 -0700

    Default filter(Request, Response)

--- src/main/java/io/vlingo/xoom/http/ResponseFilter.java
+++ src/main/java/io/vlingo/xoom/http/ResponseFilter.java
@@ -27,4 +27,19 @@ public abstract class ResponseFilter extends Filter {
    * @return {@code Tuple2<Request,Boolean>}
    */
   public abstract Tuple2<Response,Boolean> filter(final Response response);
+
+  /**
+   * Answer the {@code Response} to be propagated forward to the next {@code ResponseFilter}
+   * or as the final {@code Response}, and a {@code Boolean} indicating whether or not the
+   * chain should continue or be short circuited. If the {@code Boolean} is true, the chain
+   * will continue; if false, it will be short circuited. May use {@code request} to produce
+   * the {@code Response}.
+   * <p>Must override to implement non-default response.</p>
+   * @param request the Request, which may hold data necessary to produce the Response
+   * @param response the Response to filter
+   * @return {@code Tuple2<Request,Boolean>}
+   */
+  public Tuple2<Response,Boolean> filter(final Request request, final Response response) {
+    return filter(response);
+  }
 }
